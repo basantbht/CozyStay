aea9f4f626c4a7c4afec1e529884537fb0ff8a29
basantbht <bhattbasant012@gmail.com>
2025-05-31 16:04:58 +0545

    added routes

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
+// Core Module
+const path = require('path');
+
+// External Module
 const express = require('express');
+
+// Local Module
+const userRouter = require("./routes/userRouter");
+const hostRouter = require("./routes/hostRouter");
+const rootDir = require('./utils/pathUtil')
+
+const app = express();
+
+app.use(express.urlencoded());
+app.use(userRouter);
+app.use("/host",hostRouter);
+
+app.use((req,res,next) =>{
+     res.status(404).sendFile(path.join(rootDir,"views", "404.html"));
+});
+
+PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
